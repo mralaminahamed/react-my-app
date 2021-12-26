002a644e3075e71d7cba7f7038f46270bae67ee4
Al-Amin Ahamed <alamin@mishusoft.com>
2021-12-26 17:06:35 +0600

    Update work

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <main className="App">
-      <header className="App-header">
+    <main className="main">
+      <header className="header">
         <p>
           Hello react app
         </p>
       </header>
-      <section>
+      <section className='container'>
         This is body section
       </section>
     </main>
