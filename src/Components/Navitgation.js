2f9d44887dc9e0e5c757df94b0e2022cff4e472e
Al-Amin Ahamed <alamin@mishusoft.com>
2021-12-27 15:47:20 +0600

    Update

--- src/Components/Navitgation.js
+++ src/Components/Navitgation.js
@@ -1,8 +1,24 @@
 export default function Navigation() {
   return (
     <nav>
-      <section>Logo</section>
-      <section>menubar</section>
+      <section className="BrandArea">
+          <a href="#"><img src="https://diviedge.com/layout-design/design-005/wp-content/uploads/sites/4/2021/01/Logo.png"/>Anatomy</a>
+          </section>
+      <section className="MenuArea">
+          <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          </ul>
+          </section>
     </nav>
   );
 }
