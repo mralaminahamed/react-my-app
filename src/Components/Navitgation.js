0e652853dfec2b8aba338046d71761605f1c0dfd
Al-Amin Ahamed <alamin@mishusoft.com>
2021-12-28 14:54:45 +0600

    added SASS for this project

--- src/Components/Navitgation.js
+++ src/Components/Navitgation.js
@@ -1,24 +1,52 @@
 export default function Navigation() {
   return (
-    <nav className="navigation">
-        <section className="BrandArea">
-            <a href="#"><img src="https://diviedge.com/layout-design/design-005/wp-content/uploads/sites/4/2021/01/Logo.png"/>Anatomy</a>
-        </section>
-        <section className="MenuArea">
-            <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            </ul>
-        </section>
-    </nav>
+		<nav className="navigation">
+			<section className="BrandArea">
+				<a href="#test.html">
+					<img
+						src="https://diviedge.com/layout-design/design-005/wp-content/uploads/sites/4/2021/01/Logo.png"
+						alt="logo"
+					/>
+					Anatomy
+				</a>
+			</section>
+			<section className="MenuArea">
+				<ul>
+					<li>
+						<a href="#test.html">Home</a>
+					</li>
+					<li>
+						<a href="#test.html">Home</a>
+					</li>
+					<li>
+						<a href="#test.html">Home</a>
+					</li>
+					<li>
+						<a href="#test.html">Home</a>
+					</li>
+					<li>
+						<a href="#test.html">Home</a>
+					</li>
+					<li>
+						<a href="#test.html">Home</a>
+					</li>
+					<li>
+						<a href="#test.html">Home</a>
+					</li>
+					<li>
+						<a href="#test.html">Home</a>
+					</li>
+					<li>
+						<a href="#test.html">Home</a>
+					</li>
+					<li>
+						<a href="#test.html">Home</a>
+					</li>
+					<li>
+						<a href="#test.html">Home</a>
+					</li>
+				</ul>
+			</section>
+		</nav>
   );
 }
