3f1c0afab2f38c8b74981ad54757675df3a6f7b6
Al-Amin Ahamed <alamin@mishusoft.com>
2021-12-29 14:17:07 +0600

    working with new ideas

--- src/Assets/ts/Components/Navitgation.js
+++ src/Assets/ts/Components/Navitgation.js
@@ -1,52 +1,54 @@
 export default function Navigation() {
   return (
-		<nav className="navigation">
-			<section className="BrandArea">
-				<a href="#test.html">
-					<img
-						src="https://diviedge.com/layout-design/design-005/wp-content/uploads/sites/4/2021/01/Logo.png"
-						alt="logo"
-					/>
-					Anatomy
-				</a>
-			</section>
-			<section className="MenuArea">
-				<ul>
-					<li>
-						<a href="#test.html">Home</a>
-					</li>
-					<li>
-						<a href="#test.html">Home</a>
-					</li>
-					<li>
-						<a href="#test.html">Home</a>
-					</li>
-					<li>
-						<a href="#test.html">Home</a>
-					</li>
-					<li>
-						<a href="#test.html">Home</a>
-					</li>
-					<li>
-						<a href="#test.html">Home</a>
-					</li>
-					<li>
-						<a href="#test.html">Home</a>
-					</li>
-					<li>
-						<a href="#test.html">Home</a>
-					</li>
-					<li>
-						<a href="#test.html">Home</a>
-					</li>
-					<li>
-						<a href="#test.html">Home</a>
-					</li>
-					<li>
-						<a href="#test.html">Home</a>
-					</li>
-				</ul>
-			</section>
-		</nav>
+		<header>
+			<nav className="navigation">
+				<section className="BrandArea">
+					<a href="#test.html">
+						<img
+							src="https://diviedge.com/layout-design/design-005/wp-content/uploads/sites/4/2021/01/Logo.png"
+							alt="logo"
+						/>
+						Anatomy
+					</a>
+				</section>
+				<section className="MenuArea">
+					<ul>
+						<li>
+							<a href="#test.html">Home</a>
+						</li>
+						<li>
+							<a href="#test.html">Home</a>
+						</li>
+						<li>
+							<a href="#test.html">Home</a>
+						</li>
+						<li>
+							<a href="#test.html">Home</a>
+						</li>
+						<li>
+							<a href="#test.html">Home</a>
+						</li>
+						<li>
+							<a href="#test.html">Home</a>
+						</li>
+						<li>
+							<a href="#test.html">Home</a>
+						</li>
+						<li>
+							<a href="#test.html">Home</a>
+						</li>
+						<li>
+							<a href="#test.html">Home</a>
+						</li>
+						<li>
+							<a href="#test.html">Home</a>
+						</li>
+						<li>
+							<a href="#test.html">Home</a>
+						</li>
+					</ul>
+				</section>
+			</nav>
+		</header>
   );
 }
