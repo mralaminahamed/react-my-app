36cad26ac08ecc6b9fd2dcfffc51149ee4d84b0f
Al-Amin Ahamed <alamin@mishusoft.com>
2021-12-30 11:30:43 +0600

    Update color, navigations

--- src/Assets/ts/App.tsx
+++ src/Assets/ts/App.tsx
@@ -29,19 +29,14 @@ function App() {
 							<Route path="/" element={<Home />} />
 							<Route path="/about" element={<About />} />
 							<Route path="/products" element={<Products />} />
-
-							{/* <Route path="/" element={<Home />}>
-								<Route path="about" element={<About />} />
-								<Route path="products" element={<Products />} />
-								<Route
-									path="*"
-									element={
-										<main style={{ padding: "1rem" }}>
-											<p>There's nothing here!</p>
-										</main>
-									}
-								/>
-							</Route> */}
+							<Route
+								path="*"
+								element={
+									<main style={{ padding: "1rem" }}>
+										<p>There's nothing here!</p>
+									</main>
+								}
+							/>
 						</Routes>
 					</section>
 				</main>
